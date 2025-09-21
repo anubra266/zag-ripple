0ed4962d151516fa8ad1f29f9b1a36668d7b8ba7
anubra266 <anubra266@gmail.com>
2025-09-21 13:56:46 -0700

    gh-pages

--- site/vite.config.js
+++ site/vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
-import { ripple } from 'vite-plugin-ripple';
+import { defineConfig } from "vite";
+import { ripple } from "vite-plugin-ripple";
+
+console.log({env: process.env.NODE_ENV});
 
 export default defineConfig({
-	plugins: [ripple()],
-	server: {
-		port: 3000,
-	},
-	build: {
-		target: 'esnext',
-		outDir: "site-build",
-	}
+  plugins: [ripple()],
+  base: process.env.NODE_ENV === "production" ? "/zag-ripple/" : "/",
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: "esnext",
+    outDir: "site-build",
+  },
 });
