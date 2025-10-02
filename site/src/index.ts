b34eacfd23733f7859a2a696154dcc689a69d2af
anubra266 <anubra266@gmail.com>
2025-10-02 09:33:14 -0700

    add anallytics

--- site/src/index.ts
+++ site/src/index.ts
@@ -1,6 +1,9 @@
 import { mount } from 'ripple';
 import { Layout } from './Layout.ripple';
 import './styles/index.css';
+import { inject } from "@vercel/analytics"
+
+inject()
 
 mount(Layout, {
 	target: document.getElementById('root'),
